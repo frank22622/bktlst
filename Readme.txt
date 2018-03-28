94e561d1655e3091315be019374b2dfdfc3e6251
frank22622 <frank22622@hotmail.com>
2018-03-28 15:43:50 +0800

    Update testing

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,4 @@
 Dependency:
 flask
 flask-mysql
+gunicorn
--- tests/test_index.py
+++ tests/test_index.py
@@ -3,7 +3,9 @@ import unittest
 import sys
 
 # Append the above path to allow us to relative import the below
-sys.path.append('.')
+sys.path.append('..')
+
+from app import app
 
 class BasicTests(unittest.TestCase):
     # executed prior to each test
@@ -20,12 +22,12 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_main_page_index(self):
-        response = self.app.get('/index', follow_redirects=True)
+        response = self.app.get('/signup', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_signup_page(self):
-        response = self.app.get('/signup', follow_redirects=True)
-        self.assertEqual(response.status_code, 202)
+    def test_main_page_index(self):
+        response = self.app.get('/signin', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
