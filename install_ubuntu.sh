1a9eadfdae40ca9ceb4ea3c6be1d549b7c795c41
zhuhan <frank22622@hotmail.com>
2018-03-29 13:12:45 +0800

    add ubuntu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install python35 -y
 yum install python35-pip -y
-pip-3.5 install flask pylink flask-mysql selenium virtualenv
+pip-3.5 install flask pylink flask-mysql selenium virtualenv gunicorn
